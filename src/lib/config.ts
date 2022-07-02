4b41acb44ca21042e773ea499d849e685a277041
Lucas Tiago de Moraes <lucastiagodemoraes@gmail.com>
2022-07-01 22:12:19 -0300

    feat: add axios defaults in config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,11 @@
 export const Config = {
   url: 'https://www.youtube.com'
 }
+
+export const axiosDefaults = {
+  baseURL: Config.url,
+  timeout: 120,
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+  }
+}
