0574fd0027ddd7c4d366b834397c9047d72ac680
Lucas Tiago de Moraes <lucastiagodemoraes@gmail.com>
2022-07-01 22:12:39 -0300

    test: add axios defaults in config

--- tests/lib/config.test.ts
+++ tests/lib/config.test.ts
@@ -1,7 +1,11 @@
-import { Config } from '../../src/lib/config'
+import { Config, axiosDefaults } from '../../src/lib/config'
 
 describe('testing config file', () => {
   test('the url should be the same youtube', () => {
     expect(Config.url).toBe('https://www.youtube.com')
+  })
+
+  test('axiosDefaults should be a object', () => {
+    expect(typeof axiosDefaults).toBe('object')
   })
 })
